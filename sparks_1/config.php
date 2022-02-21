4766aaa31965dfc38108e36832bee54ff8e88c77
rajashabharish <73815785+rajashabharish@users.noreply.github.com>
2022-02-21 19:43:00 +0530

    Add files via upload

---
+++ sparks_1/config.php
@@ -0,0 +1,11 @@
+<?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = '';
+	$dbname = "sparks"; 
+
+	$conn = mysqli_connect($servername, $username, $password, $dbname) or dir("unable to connect");
+
+
+?>
---
+++ sparks_1/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Raja Bank </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Raja Bank</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="customers.php">Our Customers</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="transactions.php">Transfer History</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page"
+                            href="https://www.xe.com/currencyconverter/convert/?Amount=20&From=INR&To=EUR">Currency
+                            Convertor </a>
+                    </li>
+
+            </div>
+
+
+            </ul>
+
+        </div>
+        </div>
+    </nav>
+
+    <!-- Caraousel-->
+
+    <div id="demo" class="carousel slide" data-ride="carousel">
+
+
+    </div>
+
+    <!--About us-->
+
+    <section class="my-5">
+        <div class="py-5">
+            <h3 class="text-center">About Us</h3>
+        </div>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12">
+                    <img src="images/2.jpg" class="img-fluid">
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <h1>Raja Bank</h1>
+                    <p1> Raja Bank is a major nationalised bank. It is under the ownership of the Ministry of Finance,
+                        Government of India. It was established in 1907 and is headquartered in Chennai, India. It
+                        serves over 100 million customers with 41,620 employees, 6,004 branches with 5,428 ATMs and Cash
+                        deposit machines and is one of the top performing public sector banks in India. Total business
+                        of the bank has touched ₹930,000 crore (US$120 billion) as on 31 March 2021. Bank's Information
+                        Systems and Security processes certified with ISO27001:2013 standard and is among very few Banks
+                        certified worldwide. It has overseas branches in Colombo and Singapore including a Foreign
+                        Currency Banking Unit at Colombo and Jaffna. It has 227 Overseas Correspondent banks in 75
+                        countries. Since 1978, the Government of India has owned the bank. As per the announcement made
+                        by the Indian Finance Minister Nirmala Sitharaman on 30 August 2019, Allahabad Bank merged from
+                        1 April 2020, making it the seventh largest bank in the country.
+                </div>
+
+
+            </div>
+        </div>
+
+        <!--Dsiclaimer-->
+
+    </section>
+    <section class="my-5">
+        <div class="py-5">
+            <h3 class="text-center">Disclaimer</h3>
+            <p1 style="text-align: center;">This website is the
+                outcome of a intern project, and does not
+                necessarily represent the views of any organisation or any other individuals referenced or
+                acknowledged within the website. Anyone may reproduce, distribute, adapt, translate the content on the
+                website, without explicit permission, provided that the content is accompanied by an acknowledgement
+                that Raja Bank website is the source and that it is clearly indicated if changes were made to the
+                original content.</p1>
+        </div>
+
+
+
+
+</body>
+
+</html>
